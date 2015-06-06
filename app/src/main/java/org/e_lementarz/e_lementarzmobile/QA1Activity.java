f809875408375df83723a3323a53c11cdf0f109b
mrvalgard1 <mrvalgard@gmail.com>
2015-06-06 15:07:38 +0200

    alternative view QA

--- app/src/main/java/org/e_lementarz/e_lementarzmobile/QA1Activity.java
+++ app/src/main/java/org/e_lementarz/e_lementarzmobile/QA1Activity.java
@@ -6,7 +6,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class QA1Activity extends ActionBarActivity {
@@ -15,8 +17,24 @@ public class QA1Activity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
+        setContentView(R.layout.activity_qa1);
     }
-
+    public void onClick2003 (View view)
+    {
+        Toast.makeText(this, "Wrong! Try again", Toast.LENGTH_SHORT).show();
+    }
+    public void onClick2004 (View view)
+    {
+        Toast.makeText(this, "Wrong! Try again", Toast.LENGTH_SHORT).show();
+    }
+    public void onClick2005 (View view)
+    {
+        Toast.makeText(this, "Yes! Well done", Toast.LENGTH_SHORT).show();
+    }
+    public void onClick2006 (View view)
+    {
+        Toast.makeText(this, "Wrong! Try again", Toast.LENGTH_SHORT).show();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
