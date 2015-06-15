feef720225e3a337ccb1dab94bab711b092ba860
Damian Melniczuk <damian.melniczuk@gmail.com>
2015-06-15 15:16:03 +0200

    Display of randomly selected number.

--- app/src/main/java/org/e_lementarz/e_lementarzmobile/QA1Activity.java
+++ app/src/main/java/org/e_lementarz/e_lementarzmobile/QA1Activity.java
@@ -1,23 +1,51 @@
 package org.e_lementarz.e_lementarzmobile;
 
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Random;
+
 
 public class QA1Activity extends ActionBarActivity {
 
+    /**
+     * Returns a pseudo-random number between min and max, inclusive.
+     * The difference between min and max can be at most
+     * <code>Integer.MAX_VALUE - 1</code>.
+     *
+     * @param min Minimum value
+     * @param max Maximum value.  Must be greater than min.
+     * @return Integer between min and max, inclusive.
+     * @see java.util.Random#nextInt(int)
+     */
+    public static int randInt(int min, int max) {
+
+        // NOTE: Usually this should be a field rather than a method
+        // variable so that it is not re-seeded every call.
+        Random rand = new Random();
+
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+
+        return randomNum;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int quizPack = randInt(0, 1);
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
         setContentView(R.layout.activity_qa1);
+
+        TextView testText = (TextView) findViewById(R.id.testField);
+        testText.setText("" + quizPack);
     }
     public void onClick2003 (View view)
     {
